c6f325a993fb20dfeeba1442b86572f391a8df08
MinkieYume <git@yumieko.com>
2025-06-06 10:51:29 +0800

    www

--- sexp_neko/neko_lisp_sexp_neko/src/core.rs
+++ sexp_neko/neko_lisp_sexp_neko/src/core.rs
@@ -1,6 +1,5 @@
 use alloc::{boxed::Box,string::String, vec::Vec,rc::Rc};
 use core::cell::RefCell;
-use std::borrow::Borrow;
 use hashbrown::HashMap;
 use crate::types::Symbol;
 use crate::types::NekoType;
