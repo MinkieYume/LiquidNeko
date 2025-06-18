bbb93c5d9e4ae2282a69a6170cad56d5f433d9c6
MinkieYume <git@yumieko.com>
2025-06-18 08:34:30 +0800

    owo

--- sexp_neko/nekolisp_std/src/main.rs
+++ sexp_neko/nekolisp_std/src/main.rs
@@ -1,4 +1,22 @@
-
 fn main() {
-    println!("Hello, world!");
+    let mut env = Env::default();
+    loop {
+	let mut input = String::new();
+	print!(">>> ");
+	io::stdout().flush().unwrap();
+	let bytes_read = io::stdin().read_line(&mut input).unwrap();
+        if bytes_read == 0{
+            println!("\nByeNyan");
+            break;
+        }
+        let mut strs = reader::pre_read_str(&input,env.clone());
+        let mut results:Vec<String> = Vec::new();
+        //println!("{:?}",&strs);
+        for st in strs {
+            let s = rep(st.as_str(),env.clone());
+            results.push(st);
+            println!("{}",&s);
+        }
+	io::stdout().flush().unwrap();
+    }
 }
